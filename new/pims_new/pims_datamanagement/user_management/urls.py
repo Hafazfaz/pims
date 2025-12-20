0e97b44a61f8f8ae88ee4d899052427662f343e7
emperorsixpacks <andrewdvd10@gmail.com>
2025-12-20 21:37:30 +0100

    feat: removed otp

--- new/pims_new/pims_datamanagement/user_management/urls.py
+++ new/pims_new/pims_datamanagement/user_management/urls.py
@@ -7,6 +7,6 @@ app_name = 'user_management'
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('otp/verify/email/', views.EmailOTPVerifyView.as_view(), name='otp_verify_email'),
+    # path('otp/verify/email/', views.EmailOTPVerifyView.as_view(), name='otp_verify_email'),
     path('password/change/force/', views.ForcePasswordChangeView.as_view(), name='password_change_force'),
 ]
