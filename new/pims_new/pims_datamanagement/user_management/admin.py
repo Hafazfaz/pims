7bf6b010a7938b6cbf1b0dcdc13e2d47645a2ab8
Andrew David <andrewdvd10@gmail.com>
2026-06-08 14:10:13 +0100

    feat: add urgent priority permission checkbox to admin user creation form
    /accounts/users/create/ now shows a 'Can mark documents as
    Urgent/High Priority' checkbox under a Permissions section.

--- new/pims_new/pims_datamanagement/user_management/admin.py
+++ new/pims_new/pims_datamanagement/user_management/admin.py
@@ -1,10 +1,37 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Permission
 from organization.models import Staff
 from unfold.admin import ModelAdmin
 from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
 from .models import CustomUser
+
+
+class CustomUserAdminForm(UserCreationForm):
+    can_set_urgent_priority = forms.BooleanField(
+        required=False,
+        label="Can mark documents as Urgent/High Priority",
+    )
+
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ("can_set_urgent_priority",)
+
+    def save(self, commit=True):
+        user = super().save(commit=commit)
+        if commit:
+            if self.cleaned_data.get("can_set_urgent_priority"):
+                urgent_perm = Permission.objects.get(
+                    content_type__app_label="user_management",
+                    codename="can_set_urgent_priority",
+                )
+                user.user_permissions.add(urgent_perm)
+            else:
+                user.user_permissions.filter(
+                    codename="can_set_urgent_priority",
+                ).delete()
+        return user
 
 
 @admin.action(description="Unlock selected accounts")
@@ -18,13 +45,17 @@ def unlock_accounts(modeladmin, request, queryset):
 
 class CustomUserAdmin(ModelAdmin, UserAdmin):
     form = UserChangeForm
-    add_form = UserCreationForm
+    add_form = CustomUserAdminForm
     change_password_form = AdminPasswordChangeForm
     fieldsets = (
         *UserAdmin.fieldsets,
         (None, {"fields": ("must_change_password", "failed_login_attempts", "lockout_until")}),
     )
-    add_fieldsets = (*UserAdmin.add_fieldsets, (None, {"fields": ("must_change_password",)}))
+    add_fieldsets = (
+        *UserAdmin.add_fieldsets,
+        (None, {"fields": ("must_change_password",)}),
+        ("Permissions", {"fields": ("can_set_urgent_priority",)}),
+    )
     list_display = (*UserAdmin.list_display, "must_change_password", "failed_login_attempts", "lockout_until")
     search_fields = ("username", "first_name", "last_name", "email")
     actions = [unlock_accounts]
